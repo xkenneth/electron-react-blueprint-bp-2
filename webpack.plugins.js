4d0512df398e63eb276ddd5c4a79a5784b342a4b
Kenneth Miller <xkenneth@Bandley-M1.lan>
2021-07-27 22:41:55 -0500

    adding scichart support

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,17 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+//Added to support scichart
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = [
-  new ForkTsCheckerWebpackPlugin()
+  new ForkTsCheckerWebpackPlugin(),
+  //added to support scichart
+  new CopyPlugin({
+    patterns: [
+        { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "main_window/" }
+    ]
+  }),
 ];
