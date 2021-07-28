38f18ce4c33b571c63a35d82003a7e30c939b97f
Kenneth Miller <xkenneth@Bandley-M1.lan>
2021-07-27 23:07:19 -0500

    removed scichart

--- src/app.jsx
+++ src/app.jsx
@@ -12,35 +12,6 @@ import "../node_modules/normalize.css/normalize.css"
 import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css"
 import "../node_modules/@blueprintjs/core/lib/css/blueprint.css"
 
-//import scichart
-
-/*import {SciChartSurface} from "../node_modules/scichart/charting/Visuals/SciChartSurface";
-import {NumericAxis} from "../node_modules/scichart/charting/Visuals/Axis/NumericAxis";
-
-console.log("imported scichart")
-
-SciChartSurface.setRuntimeLicenseKey("");
-
-console.log("Set Scichart license key")
-
-async function initSciChart() {
-  // Create the SciChartSurface in the div 'scichart-root'
-  // The SciChartSurface, and webassembly context 'wasmContext' are paired. This wasmContext
-  // instance must be passed to other types that exist on the same surface.
-  const {sciChartSurface, wasmContext} = await SciChartSurface.create("scichart-root");
-  // Create an X,Y Axis and add to the chart
-  const xAxis = new NumericAxis(wasmContext);
-  const yAxis = new NumericAxis(wasmContext);
- 
-  sciChartSurface.xAxes.add(xAxis);
-  sciChartSurface.yAxes.add(yAxis);
-  // That's it! You just created your first SciChartSurface!
-}
-
-initSciChart();*/
-
-console.log("SciChart Initialized")
-
 function App () {
     const onClick = () => {
         console.log("Hello World")
